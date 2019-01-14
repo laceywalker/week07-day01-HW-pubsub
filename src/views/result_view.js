23007a03dc5776196ad3327d97b4b9428e8b06b9
lacey <hiu@example.com>
2019-01-14 18:50:24 +0000

    write formResult

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -5,6 +5,16 @@ const ResultView = function() {
 
 };
 
+ResultView.prototype.displayResult = function (result) {
+  const resultElement = document.querySelector('#result');
+  if (result === true) {
+    resultElement.textContent = `Yes, it's a prime number!`
+  }
+  else {
+    resultElement.textContent = `This is not a prime number, dumbo!`
+  }
+};
+
 
 
 module.exports = ResultView;
